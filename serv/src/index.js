3e7119d733b6643469327aa9b3be745d69f41729
LiaCabrall <lianav.cabral@gmail.com>
2024-12-01 15:00:50 -0300

    sobre + atualz footer + atualiz html

--- serv/src/index.js
+++ serv/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './global.css';
 import App from './App';
+import Sobre from './Sobre';
+import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Página inicial */}
+        <Route path="/sobre" element={<Sobre />} /> {/* Página "Sobre Nós" */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
